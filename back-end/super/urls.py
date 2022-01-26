734c55141fa2670be2b5da06436a7e392a9dca91
jerrytek <jerrycodingtek@gmail.com>
2022-01-26 10:01:33 -0600

    added auth

--- back-end/super/urls.py
+++ back-end/super/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from universe import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = routers.DefaultRouter()
 router.register('publishers', views.PublisherViewSet)
@@ -24,5 +25,8 @@ router.register('characters', views.CharacterViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-auth', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh', TokenRefreshView.as_view()),
 ]
--- back-end/universe/views.py
+++ back-end/universe/views.py
@@ -1,11 +1,14 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from universe.serializers import PublisherSerializer, CharacterSerializer
 from universe.models import Publisher, Character
 
 class PublisherViewSet(viewsets.ModelViewSet):
   queryset = Publisher.objects.all()
   serializer_class = PublisherSerializer
+  permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class CharacterViewSet(viewsets.ModelViewSet):
   queryset = Character.objects.all()
   serializer_class = CharacterSerializer
+  permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
