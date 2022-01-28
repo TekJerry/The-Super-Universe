7459ffefb0b685e680b8d89a224d56e887c64ecf
jerrytek <jerrycodingtek@gmail.com>
2022-01-28 12:12:13 -0600

    added id field to serializers

--- back-end/universe/serializers.py
+++ back-end/universe/serializers.py
@@ -5,9 +5,9 @@ class PublisherSerializer(serializers.HyperlinkedModelSerializer):
   characters = serializers.StringRelatedField(many=True, read_only=True)
   class Meta:
     model = Publisher
-    fields = ['company', 'img_url', 'founded', 'founder', 'characters']
+    fields = ['id','company', 'img_url', 'founded', 'founder', 'characters']
 
 class CharacterSerializer(serializers.HyperlinkedModelSerializer):
   class Meta: 
     model = Character
-    fields = ['code_name', 'weapon', 'fight_ability', 'real_name', 'dob', 'img_url', 'is_villain', 'is_hero', 'background', 'origin', 'publisher']
+    fields = ['id','code_name', 'weapon', 'fight_ability', 'real_name', 'dob', 'img_url', 'is_villain', 'is_hero', 'background', 'origin', 'publisher']
