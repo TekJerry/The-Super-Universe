b599eaa8fc0af00d2ae968b0ab1f59adc5a5cdb3
jerrytek <jerrycodingtek@gmail.com>
2022-01-29 14:48:17 -0600

    lets see if i can delete my accounts folder

--- front-end/super-universe/src/screens/Villains/Villains.js
+++ front-end/super-universe/src/screens/Villains/Villains.js
@@ -3,6 +3,7 @@ import Layout from "../../components/Layout/Layout";
 import "./Villains.css";
 import { useEffect, useState } from "react";
 import { grabCharacters } from "../../services/index";
+import { Link } from "react-router-dom";
 
 export default function Villains() {
   const [characters, setCharacters] = useState([]);
@@ -62,22 +63,29 @@ export default function Villains() {
             </div>
           </div>
           <div className="suggestions-villain"></div>
-        </div>
-        <div className="villain-details">
-          {" "}
+        </div>        
+          {
+            codeName.img_url ?    
+            <div className="villain-details">  
           <div className="hero-img">
             <img src={codeName.img_url} alt="villain goes here" />
           </div>
           <div className="facts">
-            <div className="name">{codeName.code_name}</div>
-            <div className="weapon">{codeName.weapon}</div>
-            <div className="fight-ability">{codeName.fight_ability}</div>
-            <div className="real-name">{codeName.real_name}</div>
-            <div className="dob">{codeName.dob}</div>
-            <div className="background">{codeName.background}</div>
-            <div className="origin">{codeName.origin}</div>
-          </div>
-        </div>
+            <div className="name"><p className="question">Villain Code Name: <span className="answer">{codeName.code_name}</span></p> </div>
+            <div className="weapon"><p className="question">Favorite Weapon: <span className="answer">{codeName.weapon}</span></p></div>
+            <div className="fight-ability"><p className="question">Fighting Ability: <span className="answer">{codeName.fight_ability}</span></p></div>
+            <div className="real-name"><p className="question">Real Name: <span className="answer">{codeName.real_name}</span></p></div>
+            <div className="dob"><p className="question">Date of Birth: <span className="answer">{codeName.dob}</span></p></div>
+            <div className="background"><p className="question">Background Story: <span className="answer">{codeName.background}</span></p></div>
+            <div className="origin"><p className="question">Origin Story: <span className="answer">{codeName.origin}</span></p></div>
+            <div className="multiple-btn">
+              <Link to={`/character/edit/${codeName.id}`}>
+                <button className="edit-character">Edit Character</button>
+              </Link>
+            </div>
+                </div>
+                </div>
+            : <div className="villain-details"></div> }
       </div>
     </Layout>
   );
