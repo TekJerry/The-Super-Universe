7e6f9f211daf5a4c645aaa8afa9e9694677ca105
jerrytek <jerrycodingtek@gmail.com>
2022-01-30 18:10:24 -0600

    edited cors origin

--- back-end/super/settings.py
+++ back-end/super/settings.py
@@ -163,4 +163,4 @@ REST_FRAMEWORK= {
 }
 
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://localhost:3001"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://localhost:3001", "https://cocky-noether-81384f.netlify.app/"]
